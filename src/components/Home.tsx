a7c1e7e4a8131338d4aeed25bbca7d1464098422
Isaac Fimbres <fimbresisaac23@gmail.com>
2025-02-06 20:23:44 -0800

    refactoring old page in a new componet

---
+++ src/components/Home.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+import { Button } from '@/components/ui/button';
+import { Checkbox } from '@/components/ui/checkbox';
+
+import { cn } from '@/lib/utils';
+
+function Home() {
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [color, setColor] = useState<'blue' | 'red'>('blue');
+
+  return (
+    <div className='h-screen w-screen bg-neutral-800 flex justify-center items-center'>
+      <h1 className='text-blue-500 font-3xl text-center'>Vite Testing</h1>
+      <Button className={cn(color === 'blue' ? 'bg-blue-500' : 'bg-red-500')} data-testid="button" disabled={isDisabled} onClick={() => setColor(c => c === 'blue' ? 'red' : 'blue')}>Test Button</Button>
+      <Checkbox data-testid="checkbox" onChange={e => setIsDisabled(!!e.currentTarget.value)} />
+    </div>
+  )
+}
+
+export default Home
